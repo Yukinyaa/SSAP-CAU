b9138884a76109f01128c1d2325b8ea0f2ffbb3f
yukinyaa <yukinyaa712@gmail.com>
2019-06-09 05:53:12 +0900

    213

--- Controllers/Go_to_action_pageController.cs
+++ Controllers/Go_to_action_pageController.cs
@@ -20,7 +20,7 @@ namespace SSAP_CAU.Controllers
                 1 + "," +//type    INT         NOT NULL,
                 register.inputAddress + "," +//addr TEXT(65535),
                 register.inputcontext + "," +//content TEXT(65535),
-                register.inputName + "," +//writer CHAR(100),       
+                register.inputName + "," +//writer CHAR(100),        
                 register.inputPassword + "," +//password CHAR(30),
                 "NULL" + "," +//startdate DATETIME,
                 "NULL" + "" +//enddate DATETIME,
