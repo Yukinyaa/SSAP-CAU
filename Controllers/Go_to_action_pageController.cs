d6264c30716768e9a80ff5f5974aac182d061f2e
yukinyaa <yukinyaa712@gmail.com>
2019-06-09 17:27:08 +0900

    이번엔 성공할거야

--- Controllers/Go_to_action_pageController.cs
+++ Controllers/Go_to_action_pageController.cs
@@ -10,9 +10,10 @@ namespace SSAP_CAU.Controllers
     {
         public IActionResult Index(RegisterViewModel register)
         {
+            
             DatabaseWrapper.SendNonQuery
             (
-                "INSERT INTO pin VALUES(" +
+                "INSERT INTO `SSAPCAU`.`pin` (`coordlat`, `coording`, `reportcnt`, `title`, `type`, `addr`, `content`, `writer`, `password`)  VALUES (" +
                 2525 + "," +//coordlat   FLOAT    NOT NULL,
                 2525 + "," +//coording   FLOAT    NOT NULL,
                 0 + "," +//reportcnt INT       NOT NULL,
@@ -21,9 +22,7 @@ namespace SSAP_CAU.Controllers
                 register.inputAddress + "," +//addr TEXT(65535),
                 register.inputcontext + "," +//content TEXT(65535),
                 register.inputName + "," +//writer CHAR(100),        
-                register.inputPassword + "," +//password CHAR(30),
-                "NULL" + "," +//startdate DATETIME,
-                "NULL" + "" +//enddate DATETIME,
+                register.inputPassword + "," +//password CHAR(30)
                 ");"
             );
 
